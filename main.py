83991174716830d5462edde4d80a8bf814aa128c
BBernYY <66414852+BBernYY@users.noreply.github.com>
2021-07-27 09:50:59 +0200

    changed the way winners get chosen!
    Now there is no exponential growth and the results are much better!

--- main.py
+++ main.py
@@ -1,13 +1,12 @@
 # import
 import random
-def iterate(times=15, amountproduce=2):
+def iterate(times=15, amountproduce=2, kidsperiter=10, alter=25):
     # define things used for agents
-    n = 5
+    n = kidsperiter * amountproduce
     data = {} # where all the agents will be stored
     names = ["Zeus", "Poseidon", "Hestia", "Hermes", "Hera", "Hephaestus", "Hades", "Dionysus", "Demeter", "Athena", "Artemis", "Ares", "Apollo", "Aphrodite"]
-    average = 0
     scores = []
-    winners = {}
+    winINFO = {}
     for i in range(n): # stores all random agents in data with random values
         data[i] = {
             "data": {
@@ -17,23 +16,21 @@ def iterate(times=15, amountproduce=2):
         }
     t = 0
     # define things used for agents
-    average = 0
-    scores = []
-    winners = {}
     for i in range(times):
+        winners = [] # reset winners
         for k, v in data.items(): # find average
             scores.append(v["data"]["score"])
-        for k, v in data.items(): # now that we found average, we can eliminate and reproduce agents.
-            if v["data"]["score"] > sum(scores) / len(scores): # if above average, reproduce.
-                winners[k] = v
+        for item in list(data.values()): # for each agent, add their score to winners
+            winners.append(item["data"]["score"])
+        winners = sorted(winners)[-kidsperiter:] # set winners to only the best of the old winners
         data = {} # kill all agents
         itn = 1
-        for k, v in winners.items(): # and replace them with new children
+        for v in winners: # and replace them with new children
             itn += 1
             for i in range(amountproduce): # for the amount of kids each surviving agent gets
                 data[max(list(data.keys()) + [0]) + 1] = { # redefine data correctly and store it in a new key
                 "data": {
-                "score": (v["data"]["score"] + random.randint(-25, 25)), # alter score a little bit
+                "score": (v + random.randint(-alter, alter)), # alter score a little bit
                 },
                 "name": random.choice(names) # new name
             }
@@ -43,6 +40,7 @@ def iterate(times=15, amountproduce=2):
 # this last part is an example
 def example(): # and it just gets the highest from 15 15gen 2child generations
     iters = []
-    for i in range(15):
-        iters.append(iterate(15)["avg"])
+    for i in range(1):
+        iters.append(iterate(100, 100, 10, 25)["avg"])
     print(max(iters))
+example()
